2e8a413e1b1de4bba3f046ce616334b3c7523fa8
CoCaBoJangLeS <shore7@gmail.com>
2020-11-17 22:51:52 -0600

    created react app and initialized yarn

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+// https://medium.com/better-programming/how-to-host-your-react-app-on-github-pages-for-free-919ad201a4cb
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
